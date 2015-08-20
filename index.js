edafb54ba10a03590c3a3d46747e90730b12d72c
Hans Fast <hans@webmapper.net>
2015-08-20 10:07:48 +0200

    initial commit of geocoder api

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var fs = require('fs');
+var nconf = require('nconf');
+nconf.file({file: './config.json'});
+var hg = require('./lib/hgapi-bridge')(nconf.get('hg'));
+var pg = require('./lib/pgapi-bridge')(nconf.get('db'));
+var queryParser = require('./lib/query-parser');
+//var auth = require('wmapi-auth');
+
+var fooYa = function(req, res, next) {
+    console.log('this handler is  being called: fooYa');
+    console.log(req.query);
+    next()
+
+}
+
+var api = express();
+
+api.use(queryParser);
+/**
+/ @api {get} /countries fetch countries based on name or id, with time filtering
+/ @apiParam {string} name name of country to search for
+*/
+function findCountries(req, res) {res.send('hello')};
+function getCountries(req, res) {res.send('hello')};
+
+api.get('/', function(req, res) {
+  res.send({
+    name: 'Demo',
+    version: '0.0.1',
+    message: 'Returning geojson and topojson'
+  });
+});
+api.get('/find', findCountries);
+api.get('/fetch', getCountries);
+api.listen(8090, function() {
+  console.log('Topojson API listening on port 8090');
+});
+
