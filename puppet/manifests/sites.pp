e25e9f873968c0ab265de3c5b4107f51f47de350
Lucien van Wouw <lwo@iisg.nl>
2015-11-25 20:58:36 +0100

    scaffolding 6

--- puppet/manifests/sites.pp
+++ puppet/manifests/sites.pp
@@ -9,9 +9,10 @@ node 'hg.home' {
 
   file {
     '/etc/environment':
-      ensure => file,
+      ensure  => file,
       content => '
       export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
+      export HISTOGRAPH_CONFIG=/opt/histograph/histograph.run.yml
       PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"' ;
   }
 
@@ -27,5 +28,25 @@ node 'hg.home' {
   create_resources(postgresql::server::pg_hba_rule, hiera('postgresql::server::pg_hba_rule', { }))
   include redis
 
+  $user = 'histograph'
+  group {
+    $user:
+      ensure => present ;
+  }
+  user {
+    $user:
+      groups     => $user,
+      managehome => true ;
+  }
 
+  file {
+    "/opt/${$user}":
+      ensure => directory,
+      owner  => $user,
+      group  => $user ;
+    '/opt/neo4j/neo4j-community-2.2.6/plugins/histograph-plugin-0.5.0-SNAPSHOT.jar':
+      ensure  => file,
+      notify  => Service['neo4j'],
+      source  => 'puppet:///modules/geoinfra/histograph-plugin-0.5.0-SNAPSHOT.jar';
+  }
 }
